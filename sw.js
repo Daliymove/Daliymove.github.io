a4b8368db1bb64b4e16d9d9608e631dff1be9563
黄杰 <920688930@qq.com>
2020-09-10 12:16:47 +0800

    Site updated: 2020-09-10 12:16:46

--- sw.js
+++ sw.js
@@ -24,25 +24,25 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/posts/55999.html');
-
   precacheUrls.push('/posts/6199.html');
-
-  precacheUrls.push('/posts/63558.html');
 
   precacheUrls.push('/posts/63306.html');
 
-  precacheUrls.push('/posts/16637.html');
+  precacheUrls.push('/posts/63558.html');
 
-  precacheUrls.push('/posts/3662.html');
+  precacheUrls.push('/posts/16637.html');
 
   precacheUrls.push('/posts/48121.html');
 
   precacheUrls.push('/posts/22223.html');
 
+  precacheUrls.push('/posts/3662.html');
+
   precacheUrls.push('/posts/61055.html');
 
   precacheUrls.push('/posts/567.html');
+
+  precacheUrls.push('/posts/55999.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
