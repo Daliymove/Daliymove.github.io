b735943fed7c832e113b28cbe5cb3975f4a2534e
黄杰 <920688930@qq.com>
2020-10-09 14:48:06 +0800

    Site updated: 2020-10-09 14:48:05

--- sw.js
+++ sw.js
@@ -24,29 +24,29 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/posts/6199.html');
-
   precacheUrls.push('/posts/55999.html');
+
+  precacheUrls.push('/posts/6199.html');
 
   precacheUrls.push('/posts/63558.html');
 
-  precacheUrls.push('/posts/3276.html');
-
   precacheUrls.push('/posts/63306.html');
-
-  precacheUrls.push('/posts/61188.html');
 
   precacheUrls.push('/posts/567.html');
 
-  precacheUrls.push('/posts/59145.html');
-
-  precacheUrls.push('/posts/52715.html');
+  precacheUrls.push('/posts/3276.html');
 
   precacheUrls.push('/posts/16637.html');
 
-  precacheUrls.push('/posts/48121.html');
+  precacheUrls.push('/posts/61188.html');
+
+  precacheUrls.push('/posts/52715.html');
+
+  precacheUrls.push('/posts/59145.html');
 
   precacheUrls.push('/posts/14131.html');
+
+  precacheUrls.push('/posts/48121.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
