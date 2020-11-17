180eb2ed0693288c19fbf903e7302e16f68d3023
黄杰 <920688930@qq.com>
2020-11-17 22:46:47 +0800

    Site updated: 2020-11-17 22:46:46

--- sw.js
+++ sw.js
@@ -26,9 +26,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/6199.html');
 
-  precacheUrls.push('/posts/63558.html');
-
   precacheUrls.push('/posts/55999.html');
+
+  precacheUrls.push('/posts/63558.html');
 
   precacheUrls.push('/posts/567.html');
 
@@ -40,13 +40,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/61188.html');
 
+  precacheUrls.push('/posts/37805.html');
+
   precacheUrls.push('/posts/16637.html');
 
   precacheUrls.push('/posts/59145.html');
 
-  precacheUrls.push('/posts/48121.html');
-
-  precacheUrls.push('/posts/3662.html');
+  precacheUrls.push('/posts/14131.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
